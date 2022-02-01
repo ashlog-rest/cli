c6277a0cdf8fae9a2d28a56dc3b423f6c3c2b4bb
Giulio De Matteis <giuliod35@gmail.com>
2022-02-02 00:58:52 +0100

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='ashlog-cli',
-    version='0.0.1',
+    version='0.0.6',
     author='Giulio De Matteis',
     description='A simple command line interface for AshLog.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ashlog-rest/cli',
     project_urls={
         "Bug Tracker": "https://github.com/ashlog-rest/cli/issues",
     },
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    packages=[
+        'ashlog',
+        'ashlog.commands',
+        'common',
+    ],
     install_requires=[
         'click',
         'keyring',
